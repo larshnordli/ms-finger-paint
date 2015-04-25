394261c43027eeb9aa5128cf0e88af18d3adad92
Lars Henrik Nordli <larhenrik91@gmail.com>
2015-04-25 20:30:05 +0200

    updated cpp

--- ms-finger-paint/ms-finger-paint/webcam.cpp
+++ ms-finger-paint/ms-finger-paint/webcam.cpp
@@ -52,37 +52,37 @@ int main(int argc, char** argv){
 	//setMouseCallback("MS Finger Paint", CallBackFunc, NULL);
 	namedWindow("Control", CV_WINDOW_NORMAL);
 
-	int lowH = 111;
-	int highH = 123;
+	int minYCC1 = 53;
+	int minYCC2 = 133;
+	int minYCC3 = 110;
 
-	int lowS = 98;
-	int highS = 255;
+	int maxYCC1 = 97;
+	int maxYCC2 = 158;
+	int maxYCC3 = 142;
 
-	int lowV = 62;
-	int highV = 255;
-
-	int avgBlur = 17;
-	int depth = 145;
+	int gauss_blur = 5;
+	int depth = 88;
+	int ellipse_size = 3;
 
 	//Create trackbars in "Control" window
-	cvCreateTrackbar("LowH", "Control", &lowH, 179);
-	cvCreateTrackbar("HighH", "Control", &highH, 179);
+	cvCreateTrackbar("minYCC1", "Control", &minYCC1, 255);
+	cvCreateTrackbar("minYCC2", "Control", &minYCC2, 255);
+	cvCreateTrackbar("minYCC3", "Control", &minYCC3, 255);
 
-	cvCreateTrackbar("LowS", "Control", &lowS, 255);
-	cvCreateTrackbar("HighS", "Control", &highS, 255);
+	cvCreateTrackbar("maxYCC1", "Control", &maxYCC1, 255);
+	cvCreateTrackbar("maxYCC2", "Control", &maxYCC2, 255);
+	cvCreateTrackbar("maxYCC3", "Control", &maxYCC3, 255);
 
-	cvCreateTrackbar("LowV", "Control", &lowV, 255);
-	cvCreateTrackbar("HighV", "Control", &highV, 255);
-
-	cvCreateTrackbar("Averaging", "Control", &avgBlur, 101);
+	cvCreateTrackbar("Gaussian", "Control", &gauss_blur, 20);
 	cvCreateTrackbar("Depth", "Control", &depth, 300);
+	//cvCreateTrackbar("EllipseSize", "Control", &ellipse_size, 20);
 
 	while (true){ //Create infinte loop for live streaming
 
 		Mat frame;
-		Mat frameHSV;
+		Mat frameYCC;
 		Mat frameThreshold;
-		Mat ellipse = getStructuringElement(MORPH_ELLIPSE, Size(5, 5));
+		Mat ellipse = getStructuringElement(MORPH_ELLIPSE, Size(3, 3));
 		RNG rng(12345);
 		vector<Vec4i> hierarchy;
 
@@ -92,21 +92,18 @@ int main(int argc, char** argv){
 			cout << "Cannot read stream" << endl;
 			break;
 		}
-
-		//frame = imread("image.jpg", CV_LOAD_IMAGE_COLOR);
-
 		//mirror image
 		flip(frame, frame, 1);
-
-		//convert RGB -> HSV
-		cvtColor(frame, frameHSV, CV_RGB2HSV);
-
-		//average blur
-		blur(frameHSV, frameHSV, Size(avgBlur, avgBlur));
-
+		//gaussian blur
+		Size kSize;
+		kSize.height = gauss_blur;
+		kSize.width = gauss_blur;
+		double sigma = 0.3*(3 / 2 - 1) + 0.8;
+		GaussianBlur(frame, frame, kSize, sigma, 0.0, 4);
+		//convert RGB -> VCC
+		cvtColor(frame, frameYCC, CV_BGR2YCrCb);
 		//threshold
-		inRange(frameHSV, Scalar(lowH, lowS, lowV), Scalar(highH, highS, highV), frameThreshold);
-
+		inRange(frameYCC, Scalar(minYCC1, minYCC2, minYCC3), Scalar(maxYCC1, maxYCC2, maxYCC3), frameThreshold); //CV_THRESH_OTSU?
 		//open and close
 		erode(frameThreshold, frameThreshold, ellipse);
 		dilate(frameThreshold, frameThreshold, ellipse);
@@ -115,23 +112,22 @@ int main(int argc, char** argv){
 
 		//find contours
 		vector<vector <Point> > contours;
-		Mat contourOutput = frameThreshold.clone();
-
-		findContours(contourOutput, contours, CV_RETR_LIST, CV_CHAIN_APPROX_NONE);
+		findContours(frameThreshold, contours, CV_RETR_LIST, CV_CHAIN_APPROX_NONE);
 
 		//hull convex and convex defects
 		vector<vector<Point> > hull(contours.size());
 		vector<vector<Point> > hull_indice_points(contours.size());
 		vector<vector<int> > hull_indices(contours.size());
+
 		int num_fingers = 0;
 		Point cursor;
 		vector<vector<Vec4i> > defects(contours.size());
-		Mat drawing = Mat::zeros(contourOutput.size(), CV_8UC3);
+		Mat drawing = Mat::zeros(frameThreshold.size(), CV_8UC3);
 
 		for (int i = 0; i < contours.size(); i++){
-			convexHull(Mat(contours[i]), hull[i], false);
-			convexHull(Mat(contours[i]), hull_indices[i], false);
-			if (hull_indices[i].size() > 3)
+			convexHull(Mat(contours[i]), hull[i], false); //for drawing
+			convexHull(Mat(contours[i]), hull_indices[i], false); // for calculations
+			if (hull_indices[i].size() > 3) // for start, end and furthest points
 				convexityDefects(Mat(contours[i]), hull_indices[i], defects[i]);
 
 			//find defects
@@ -147,14 +143,13 @@ int main(int argc, char** argv){
 					int start = defects[i][j][0];
 					int end = defects[i][j][1];
 					int furthest = defects[i][j][2];
-					//cursor.x = start;
-					//cursor.y = end;
 					hull_indice_points[i].push_back(contours[i][furthest]);
 
-					//circle(drawing, contours[i][start], 5, Scalar(0,255, 100), -1);
-					cursor = contours[i][end];
+					circle(drawing, contours[i][start], 5, Scalar(255, 0, 0), -1);
 					circle(drawing, contours[i][end], 5, Scalar(0, 255, 0), -1);
 					circle(drawing, contours[i][furthest], 5, Scalar(0, 0, 255), -1);
+
+					cursor = contours[i][end];
 
 					line(drawing, contours[i][furthest], contours[i][start], Scalar(0, 0, 255), 1);
 					line(drawing, contours[i][furthest], contours[i][end], Scalar(0, 0, 255), 1);
@@ -168,27 +163,30 @@ int main(int argc, char** argv){
 			drawContours(drawing, contours, i, color, 1, 8, vector<Vec4i>(), 0, Point());
 		}
 
+		double area0 = contourArea(contours);
+		vector<Point> approx;
+		approxPolyDP(contours, approx, 5, true);
+		double area1 = contourArea(approx);
+
 		//cursor operations
 		if (num_fingers == 1){
-			mouseLeftClick(cursor.x, cursor.y, 1);
 			SetCursorPos(cursor.x, cursor.y);
+			cout << "area0: " << area0 << endl <<
+				"area1: " << area1 << endl <<
+				"approx poly vertices" << approx.size() << endl;
 		}
-		//if (num_fingers == 2){
-		//	usleep(500);
-		//	mouseLeftClick(cursor.x, cursor.y, 0);
-		//}
 
-		imshow("MS Finger Paint", drawing); //Show image frames on created window 
+		imshow("MS Finger Paint", contours); //Show image frames on created window 
 
 		if (waitKey(30) == 27){
-			cout << "lowH: " << lowH << endl;
+			/*cout << "lowH: " << lowH << endl;
 			cout << "highH: " << highH << endl;
 			cout << "lowS: " << lowS << endl;
 			cout << "highS: " << highS << endl;
 			cout << "lowV: " << lowV << endl;
 			cout << "highV: " << highV << endl;
 			cout << "blur: " << avgBlur << endl;
-			cout << "depth: " << depth << endl;
+			cout << "depth: " << depth << endl;*/
 			cout << "Initiated quit by user" << endl;
 			break;
 		}
